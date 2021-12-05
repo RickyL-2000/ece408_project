28e0da13540e2aafa8be48cd26382fe60dd652b9
RickyL-2000 <ruiqili4@illinois.edu>
2021-12-05 22:35:10 +0800

    combine ref and debug

--- opts/new-forward-opt3.cu
+++ opts/new-forward-opt3.cu
@@ -1,14 +1,11 @@
-// Opt3
-
 #include <cmath>
 #include <iostream>
 #include "gpu-new-forward.h"
 #include <cuda_fp16.h>
 
-#define TEST_NAME "opt3 1000"
+#define TEST_NAME "opt3 10000"
 
-#define BLOCK_WIDTH 16
-#define CONV_DEBUG
+#define BLOCK_WIDTH 16 
 
 __global__ void conv_forward_kernel(
     float *y, const float *x, const float *k, 
@@ -46,27 +43,23 @@ __global__ void conv_forward_kernel(
 #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
     // Insert your GPU convolution kernel code here
-
-    int W_num = ceil(W_out / (BLOCK_WIDTH * 1.0));
-        // H_num = ceil(H_out / (BLOCK_WIDTH * 1.0));
-    int b = blockIdx.x, m = blockIdx.y;
-    int h = (blockIdx.z / W_num) * BLOCK_WIDTH + threadIdx.y,   // why is this y???? not x???
-        w = (blockIdx.z % W_num) * BLOCK_WIDTH + threadIdx.x;
-    // int h = threadIdx.y, w = threadIdx.x;
-
-    int c, p, q;
-    // define half precision float
+    int W_grid = ceil(W_out / (BLOCK_WIDTH*1.0) ); // Number of horizontal tiles per output feature map
+    int n = blockIdx.x, m = blockIdx.y;
+    int h = (blockIdx.z / W_grid) * BLOCK_WIDTH + threadIdx.y, 
+        w = (blockIdx.z % W_grid) * BLOCK_WIDTH + threadIdx.x;
+        
     float r = 0;
     half res = __float2half(r);
-    if (w >= W_out || h >= H_out) return;
-    for (c = 0; c < C; ++c) {
-        for (p = 0; p < K; ++p) {
-            for (q = 0; q < K; ++q) {
-                res += __hadd(res, __hmul(__float2half(x4d(b, c, h+p, w+q)), __float2half(k4d(m, c, p, q))));
-            }
+    
+    if (w >= W_out or h >= H_out) return;
+    for (int c = 0; c < C; ++c) {
+        for (int p = 0; p < K; ++p) {
+            for (int q = 0; q < K; ++q)
+                res = __hadd(res, __hmul(__float2half(x4d(n,c,h+p,w+q)), __float2half(k4d(m,c,p,q))));
         }
     }
-    y4d(b, m, h, w) = __half2float(res);
+        
+    y4d(n, m, h ,w) = __half2float(res);
 
 #undef y4d
 #undef x4d
@@ -91,21 +84,21 @@ __host__ void GPUInterface::conv_forward_gpu_prolog(
     //     std::cout<<"CUDA error: "<<cudaGetErrorString(error)<<std::endl;
     //     exit(-1);
     // }
-
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
 
-    const int y_size = B*M*H_out*W_out*sizeof(float), 
-              x_size = B*C*H*W*sizeof(float), 
-              k_size = M*C*K*K*sizeof(float);
-    cudaMalloc((void**) device_y_ptr, y_size);
-    cudaMalloc((void**) device_x_ptr, x_size);
-    cudaMalloc((void**) device_k_ptr, k_size);
+    int x_size = B*C*H*W*sizeof(float);
+    int y_size = B*M*H_out*W_out*sizeof(float);
+    int k_size = M*C*K*K*sizeof(float);
 
-    cudaMemcpy(*device_y_ptr, host_y, y_size, cudaMemcpyHostToDevice);
-    cudaMemcpy(*device_x_ptr, host_x, x_size, cudaMemcpyHostToDevice);
-    cudaMemcpy(*device_k_ptr, host_k, k_size, cudaMemcpyHostToDevice);
+    cudaMalloc((void **) device_x_ptr, x_size); 
+    cudaMalloc((void **) device_y_ptr, y_size); 
+    cudaMalloc((void **) device_k_ptr, k_size);
     
+    cudaMemcpy(*device_x_ptr, host_x, x_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(*device_y_ptr, host_y, y_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(*device_k_ptr, host_k, k_size, cudaMemcpyHostToDevice);
+
 #ifdef CONV_DEBUG
     cudaError_t error = cudaGetLastError();
     if(error != cudaSuccess)
@@ -117,32 +110,31 @@ __host__ void GPUInterface::conv_forward_gpu_prolog(
 }
 
 
-__host__ void GPUInterface::conv_forward_gpu(
-    float *device_y, const float *device_x, const float *device_k, 
-    const int B, const int M, const int C, const int H, const int W, const int K)
+__host__ void GPUInterface::conv_forward_gpu(float *device_y, const float *device_x, const float *device_k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
     // Set the kernel dimensions and call the kernel
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
 
-    const int W_num = ceil(W_out / (BLOCK_WIDTH * 1.0)),
-              H_num = ceil(H_out / (BLOCK_WIDTH * 1.0));
+    int W_grid = ceil(W_out / (BLOCK_WIDTH*1.0)), 
+        H_grid = ceil(H_out / (BLOCK_WIDTH*1.0)), 
+        Z = W_grid * H_grid;
 
     std::cout << "TEST NAME: " << TEST_NAME << std::endl;
 
 #ifdef CONV_DEBUG
     // print dimension information
-    std::cout << "Grid Dimension: " << B << " x " << M << " x " << W_num * H_num << std::endl;
+    std::cout << "Grid Dimension: " << B << " x " << M << " x " << W_grid * H_grid << std::endl;
     std::cout << "Block Dimension: " << BLOCK_WIDTH << " x " << BLOCK_WIDTH << " x " << 1 << std::endl;
     std::cout << "Kernel Dimension: " << M << " x " << C << " x " << K << " x " << K << std::endl;
     std::cout << "Image Dimension: " << B << " x " << C << " x " << H << " x " << W << std::endl;
     std::cout << "Output Dimension: " << B << " x " << M << " x " << H_out << " x " << W_out << std::endl;
 #endif
 
-    dim3 dimGrid(B, M, W_num * H_num);
+    dim3 dimGrid(B, M, Z);
     dim3 dimBlock(BLOCK_WIDTH, BLOCK_WIDTH, 1);
 
-    conv_forward_kernel<<<dimGrid, dimBlock>>>(device_y, device_x, device_k, B, M, C, H, W, K);
+    conv_forward_kernel<<<dimGrid, dimBlock>>> (device_y, device_x, device_k, B, M, C, H, W, K);
 
     cudaDeviceSynchronize();
 
@@ -157,21 +149,20 @@ __host__ void GPUInterface::conv_forward_gpu(
 }
 
 
-__host__ void GPUInterface::conv_forward_gpu_epilog(
-    float *host_y, float *device_y, float *device_x, float *device_k, 
-    const int B, const int M, const int C, const int H, const int W, const int K)
+__host__ void GPUInterface::conv_forward_gpu_epilog(float *host_y, float *device_y, float *device_x, float *device_k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
     // Copy the output back to host
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
     int y_size = B*M*H_out*W_out*sizeof(float);
-        // x_size = B*C*H*W*sizeof(float), 
-        // k_size = M*C*K*K*sizeof(float);
+
     cudaMemcpy(host_y, device_y, y_size, cudaMemcpyDeviceToHost);
+
+    cudaFree(device_x);  
+    cudaFree(device_y);  
+    cudaFree(device_k); 
     // Free device memory
-    cudaFree(device_y);
-    cudaFree(device_x);
-    cudaFree(device_k);
+
 }
 
 
